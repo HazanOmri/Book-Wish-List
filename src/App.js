f288170360b3e29d3edbcd71a342876b98065936
Inbal Avidov <inbal.avidov@gmail.com>
2023-02-05 14:37:45 +0200

    Added home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { Route, Router, Routes } from "react-router-dom";
+import { Route, Router, Routes } from "react-router-dom"
+import { bookService } from "./services/book.service"
 
 export function App() {
   return (
     <Router>
       <main className="main-app">
         <Routes>
-          <Route />
+          <Route element={BookIndex} path={'/'} />
         </Routes>
       </main>
-    </Router>
+    // </Router>
   )
 }
