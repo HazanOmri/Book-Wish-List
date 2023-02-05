e69687f46c3a320dd35326bcdff2681a2f91d5d6
Inbal Avidov <inbal.avidov@gmail.com>
2023-02-05 14:18:41 +0200

    Merge

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+
+import { App } from './App'
+import { store } from './store/store'
+import './assets/style/_main.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <App />
+  <Provider store={store} >
+        <App />
+    </Provider>
 )
